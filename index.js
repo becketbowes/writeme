a81f1b6d77db82b7228da7d8d7edb8689fe00391
becket <becket.bowes@gmail.com>
2022-02-06 19:31:15 -0500

    initial recommit

---
+++ index.js
@@ -0,0 +1,33 @@
+// TODO: Include packages needed for this application
+//move this?
+// const inquirer = require('inquirer');
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+
+
+// trying to fix a git issue - erase this later
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
